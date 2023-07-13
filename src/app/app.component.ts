25e759a30e09de9e4b078b06b26a4c6d38757520
Abhishek Magdum <abhishek.magdum.07@gmail.com>
2023-07-13 16:08:30 +0530

    Added : Applied validation for users name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,8 @@ export class AppComponent {
   title = 'helloword-angular-app';
   imgUrl = "../assets/BridgeLabz New Logo.svg";
   url = "https://www.bridgelabz.com";
-  userName: String = "";
-  
+  userName: string = "";
+  nameError: string ="";
   ngOnInit():void {
     this.title = "Hello from Bridgelabz."
     
@@ -20,4 +20,16 @@ export class AppComponent {
     console.log("Save button is clicked!", $event)
     window.open(this.url, "_blank");
   }
+
+  onInput($event: any ){
+    console.log("Change Enent Occurred!", $event.data);
+    const nameRegex = RegExp('^[A-Z][a-zA-Z]{2,}$');
+    if (nameRegex.test(this.userName)){
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect!";
+  }
+
+  
 }
